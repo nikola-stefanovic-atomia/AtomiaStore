ff0734061547a12494d5d7f523ab90724a6339d5
Nikola Stefanović <40986668+nikola-stefanovic-atomia@users.noreply.github.com>
2019-09-25 11:37:10 +0200

    Add option to set label for hosting packages (#80)
    Added possibility to set "Label" custom attribute on ordered hosting
    package item. The custom attribute is used to distinguish packages in
    the multi-package environment.
    
    Configuration option "EnablePackageLabel" should be set to "true" to
    allow setting a label on the package subscription and the order line.
    Also, "multiPackage" option should be enabled in the billing API to
    allow setting a label.
    
    Added HTML input field to set a label for the selected package. The field
    is visible on the page only if there are no domains in the cart. If
    the cart has a domain, the field will not be visible nor the label will be
    set on the hosting package item.
    
    Resolves PROD-1975
    
    ChangeLog:
    * [NEW] - Added option to set a label for the ordered package.

--- Atomia.Store.AspNetMvc/Models/ProductListingViewModel.cs
+++ Atomia.Store.AspNetMvc/Models/ProductListingViewModel.cs
@@ -1,4 +1,8 @@
 ﻿
+using Atomia.Store.AspNetMvc.Helpers;
+using Atomia.Store.Core;
+using System.Web.Mvc;
+
 namespace Atomia.Store.AspNetMvc.Models
 {
     /// <summary>
@@ -6,6 +10,11 @@ namespace Atomia.Store.AspNetMvc.Models
     /// </summary>
     public class ProductListingViewModel
     {
+        /// <summary>
+        /// The package provider instance
+        /// </summary>
+        private IPackageProvider packageProvider = DependencyResolver.Current.GetService<IPackageProvider>();
+
         /// <summary>
         /// Query to use for listing products
         /// </summary>
@@ -15,5 +24,17 @@ namespace Atomia.Store.AspNetMvc.Models
         /// Name of <see cref="Atomia.Store.Core.IProductListProvider" to use. />
         /// </summary>
         public virtual string ListingType { get; set; }
+
+        /// <summary>
+        /// Indicates whether customers can set a label for the package
+        /// </summary>
+        public virtual bool IsPackageLabelEnabled
+        {
+            get
+            {
+                return ConfigurationHelper.IsPackageLabelEnabled()
+                    && packageProvider.IsMultiPackageEnabled();
+            }
+        }
     }
 }
