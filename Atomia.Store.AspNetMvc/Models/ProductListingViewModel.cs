c550a44d62c76acb5a1ea6729a49cf0cb63961b9
Nikola Stefanović <40986668+nikola-stefanovic-atomia@users.noreply.github.com>
2019-09-26 15:42:34 +0200

    Revert "Add option to set label for hosting packages (#80)"
    This reverts commit ff0734061547a12494d5d7f523ab90724a6339d5.

--- Atomia.Store.AspNetMvc/Models/ProductListingViewModel.cs
+++ Atomia.Store.AspNetMvc/Models/ProductListingViewModel.cs
@@ -1,8 +1,4 @@
 ﻿
-using Atomia.Store.AspNetMvc.Helpers;
-using Atomia.Store.Core;
-using System.Web.Mvc;
-
 namespace Atomia.Store.AspNetMvc.Models
 {
     /// <summary>
@@ -10,11 +6,6 @@ namespace Atomia.Store.AspNetMvc.Models
     /// </summary>
     public class ProductListingViewModel
     {
-        /// <summary>
-        /// The package provider instance
-        /// </summary>
-        private IPackageProvider packageProvider = DependencyResolver.Current.GetService<IPackageProvider>();
-
         /// <summary>
         /// Query to use for listing products
         /// </summary>
@@ -24,17 +15,5 @@ namespace Atomia.Store.AspNetMvc.Models
         /// Name of <see cref="Atomia.Store.Core.IProductListProvider" to use. />
         /// </summary>
         public virtual string ListingType { get; set; }
-
-        /// <summary>
-        /// Indicates whether customers can set a label for the package
-        /// </summary>
-        public virtual bool IsPackageLabelEnabled
-        {
-            get
-            {
-                return ConfigurationHelper.IsPackageLabelEnabled()
-                    && packageProvider.IsMultiPackageEnabled();
-            }
-        }
     }
 }
